b747f56639e75563d066673a874bc976baed9710
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-05-21 17:24:31 +0300

    tweak(files): Changed file structure
    added more organisation :P

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,11 +7,11 @@ version '1.0'
 repository 'https://github.com/n0tsiege/SIEGE-2021-PACK'
 
 files {
-    'data/**/*.meta'
+    'data/**/**/*.meta'
 }
 
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/**/*/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/*/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/*/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/*/carvariations.meta'
