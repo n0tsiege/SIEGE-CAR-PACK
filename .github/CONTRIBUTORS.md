2d5e3a61e0dc8aa020069e33f66d494d551cae90
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-05-22 15:09:47 +0300

    tweak(naming): New name structure by Hamy-os + 3 changes
    * tweak(Issue): Made the issue template better
    
    * tweak(files): Changed file structure
    
    added more organisation :P
    
    * tweak(contributors) Added myself :(

--- .github/CONTRIBUTORS.md
+++ .github/CONTRIBUTORS.md
@@ -1,2 +1,5 @@
 ROCKY_southpaw + NonStopGamer
 - Fixed fxmanifest issue (`files` not being present)
+
+Hamy-os
+- Re-Organized files to make a better file structure :P
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,13 +7,13 @@ version '1.0'
 repository 'https://github.com/n0tsiege/SIEGE-2021-PACK'
 
 files {
-    'data/**/*.meta'
- }
- 
-data_file 'HANDLING_FILE' 'data/**/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+    'data/**/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' 'data/**/*/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/*/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/*/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/*/carvariations.meta'
 
 client_script 'vehicle_names.lua'
