81c1e898574d914bbd345366cfcfd8b8ac569525
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-05-27 16:41:50 +0300

    tweak(sounds)Changed sounds to be more pleasing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,19 +3,20 @@ game 'gta5'
 
 author 'n0tsiege'
 description 'A pack of 2021 vehicles, compiled and released by siege.'
-version '1.0'
+version '1.0.0'
 repository 'https://github.com/n0tsiege/SIEGE-2021-PACK'
 
 files {
-    'data/**/**/*.meta'
+    'data/**/**/*.*',
     'audio/sfx/STREAMED_VEHICLES_GRANULAR/suv_1_eur_v6.awc',
+    'vehicle_names.lua'
 }
 
-data_file 'HANDLING_FILE' 'data/**/*/handling.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/*/vehiclelayouts.meta'
-data_file 'VEHICLE_METADATA_FILE' 'data/**/*/vehicles.meta'
-data_file 'CARCOLS_FILE' 'data/**/*/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'data/**/*/carvariations.meta'
+data_file 'HANDLING_FILE' 'data/**/**/handling.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/**/carvariations.meta'
 data_file 'AUDIO_WAVEPACK' 'audio/sfx/STREAMED_VEHICLES_GRANULAR'
 
 client_script 'vehicle_names.lua'
