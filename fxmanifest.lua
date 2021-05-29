c5f20620ceafd2ab77a68bfb110b9943ce25de7c
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-05-29 18:40:07 +0300

    feat(sound) Replaced super, sports and muscle car audio files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -17,6 +17,6 @@ data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/**/vehiclelayouts.meta'
 data_file 'VEHICLE_METADATA_FILE' 'data/**/**/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/**/**/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/**/**/carvariations.meta'
-data_file 'AUDIO_WAVEPACK' 'audio/sfx/STREAMED_VEHICLES_GRANULAR'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/STREAMED_VEHICLES_GRANULAR/'
 
 client_script 'vehicle_names.lua'
