2ab3b6c7074937b36b8acde3e925c48383c09b86
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-06-02 08:19:06 +0300

    tweak(server) Added resourcename checker + version bump

--- sv.lua
+++ sv.lua
@@ -1,12 +1,17 @@
+SetConvarServerInfo('tags', 'NS_CAR_PACK')
 Citizen.CreateThread( function()
-    resourceName = "SIEGE-CAR-PACK ("..GetCurrentResourceName()..")"
+    resourceName = "NS-CAR-PACK ("..GetCurrentResourceName()..")"
     
     function checkVersion(err,responseText, headers)
         curVersion = LoadResourceFile(GetCurrentResourceName(), "version") 
-    
+        if GetCurrentResourceName() ~= "NS-CAR-PACK" then
+            print("-----------------------------------------------------")
+            print("Please Dont Change the resource name to avoid errors.")
+            print("-----------------------------------------------------")
+        end
         if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
  print("\n^1###############################")
-            print("\n"..resourceName.." is outdated, should be: "..responseText.."but is: "..curVersion.." ^1please update it from https://github.com/notsiege-development/SIEGE-CAR-PACK")
+            print("\n"..resourceName.." is outdated, should be: "..responseText.."but is: "..curVersion.." ^1please update it from https://github.com/notsiege-development/NS-CAR-PACK")
             print("\n^1###############################")
         elseif tonumber(curVersion) > tonumber(responseText) then
             print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
@@ -15,5 +20,5 @@ Citizen.CreateThread( function()
         end
     end
     
-    PerformHttpRequest("https://raw.githubusercontent.com/notsiege-development/SIEGE-CAR-PACK/main/version", checkVersion, "GET")
+    PerformHttpRequest("https://raw.githubusercontent.com/notsiege-development/NS-CAR-PACK/main/version", checkVersion, "GET")
     end)
